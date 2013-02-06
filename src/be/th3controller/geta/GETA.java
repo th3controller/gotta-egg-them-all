849a1654528232e950938f15e44a6a7497ccb6c8
th3controller <th3controller2000@gmail.com>
2013-02-05 20:18:24 -0500

    Changed the way the code handles mobs Small change on the enable and disable messages

--- src/be/th3controller/geta/GETA.java
+++ src/be/th3controller/geta/GETA.java
@@ -14,25 +14,23 @@ public class GETA extends JavaPlugin {
 	Logger log = Logger.getLogger("Minecraft");
 	PluginDescriptionFile pdfile;
 	
+	public void lm(String msg){
+		log.info("[GETA] " + msg);
+	}
 	public void onEnable(){
-		getServer().getPluginManager().registerEvents(new GETAListener(), this);
-		log.info(this + " is now enabled!");
+		getServer().getPluginManager().registerEvents(new GETAListener(this), this);
+		lm("Successfully initiated the plugin!");
 		pdfile = getDescription();
 	}
 
 	public void onDisable(){
-		log.info(this + " is now disabled!");
+		lm("Successfully initiated the plugin!");
 	}
-	
-	public void LogMessage(String msg){
-		this.log.info("[" + pdfile.getName() + "] " + msg);
-	}
-	
 	public boolean onCommand(CommandSender sender, Command cmd, String Label, String[] args){
 		if(cmd.getName().equalsIgnoreCase("geta")){
 			if(!(sender instanceof Player)){
-				LogMessage("You are running version" + this.pdfile.getVersion() + "of GETA(Gotta Egg Them All)");
-				LogMessage("licensed with GPLv3");
+				lm("You are running version" + this.pdfile.getVersion() + "of GETA(Gotta Egg Them All)");
+				lm("licensed with GPLv3");
 			} else {
 				((Player)sender).sendMessage(ChatColor.GREEN+ "You are running version" + this.pdfile.getVersion() + "of GETA(Gotta Egg Them All)");
 			}
--- src/be/th3controller/geta/GETAListener.java
+++ src/be/th3controller/geta/GETAListener.java
@@ -8,148 +8,145 @@ import org.bukkit.entity.Bat;
 import org.bukkit.entity.Blaze;
 import org.bukkit.entity.CaveSpider;
 import org.bukkit.entity.Chicken;
+import org.bukkit.entity.Cow;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Enderman;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Ghast;
+import org.bukkit.entity.MagmaCube;
+import org.bukkit.entity.MushroomCow;
 import org.bukkit.entity.Ocelot;
 import org.bukkit.entity.Pig;
 import org.bukkit.entity.PigZombie;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Sheep;
+import org.bukkit.entity.Silverfish;
+import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.Slime;
+import org.bukkit.entity.Spider;
 import org.bukkit.entity.Squid;
 import org.bukkit.entity.Villager;
+import org.bukkit.entity.Witch;
 import org.bukkit.entity.Wolf;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
-public class GETAListener implements Listener{
+public class GETAListener implements Listener {
 	
+	GETA plugin;
+	
+	public GETAListener(GETA plugin) {
+		this.plugin = plugin;
+	}
+	/**
+	 * 
+	 * @param perm Permission to be used
+	 * @param p Player involved
+	 * @return Returns true if the player has permission
+	 */
+	private boolean hasPerms(String perm, Player p) {
+		return p.hasPermission(perm);
+	}
+	/**
+	 * Spawns an instance of an entity
+	 * @param p The target player
+	 * @param rc The entity that is involved
+	 * @param loc The location where effects of smoke would occur
+	 * @param inv The target inventory
+	 * @param mobname The name that should be displayed when an entity is caught
+	 * @param entid The entity ID
+	 */
 	@SuppressWarnings("deprecation")
+	public void spawninstance(Player p, Entity rc, Location loc, PlayerInventory inv, String mobname, Short entid) {
+		rc.remove();
+		inv.addItem(new ItemStack(Material.MONSTER_EGG, 1, entid));
+		loc.getWorld().playEffect(loc, Effect.SMOKE, 4);
+		p.updateInventory();
+		p.sendMessage(ChatColor.GREEN+"You caught a "+mobname+"!");
+	}
+	
 	@EventHandler
-	public void PlayerInteractingEntity(PlayerInteractEntityEvent event){
+	public void PlayerInteractingEntity(PlayerInteractEntityEvent event) {
 		Player p = event.getPlayer();
 		Entity rightclick = event.getRightClicked();
+		String nameent = rightclick.getType().getName();
+		short endid = rightclick.getType().getTypeId();
 		Location loc = rightclick.getLocation();
 		PlayerInventory inventory = p.getInventory();
-		if(rightclick instanceof Creeper && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.creeper", p)){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)50));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a creeper!");
+		if(p.getItemInHand().getType() == Material.STICK) {
+			if(rightclick instanceof Bat && hasPerms("geta.bat", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Chicken && hasPerms("geta.chicken", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Cow && hasPerms("geta.cow", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof MushroomCow && hasPerms("geta.mooshroom", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Ocelot && hasPerms("geta.ocelot", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Pig && hasPerms("geta.pig", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Sheep && hasPerms("geta.sheep", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Squid && hasPerms("geta.squid", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Villager && hasPerms("geta.villager", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Enderman && hasPerms("geta.enderman", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Wolf && hasPerms("geta.wolf", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof PigZombie && hasPerms("geta.pigzombie", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Blaze && hasPerms("geta.blaze", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof CaveSpider && hasPerms("geta.cavespider", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Creeper && hasPerms("geta.creeper", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Ghast && hasPerms("geta.ghast", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof MagmaCube && hasPerms("geta.magmacube", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Silverfish && hasPerms("geta.silverfish", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Skeleton && hasPerms("geta.skeleton", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Slime && hasPerms("geta.slime", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Spider && hasPerms("geta.spider", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Witch && hasPerms("geta.witch", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
+			else if(rightclick instanceof Zombie && hasPerms("geta.zombie", p)) {
+				spawninstance(p, rightclick, loc, inventory, nameent, endid);
+			}
 		}
-		else if(rightclick instanceof Ghast && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.ghast", p)){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)56));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a ghast!");
-		}
-		else if(rightclick instanceof PigZombie && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.pigzombie", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)57));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a zombie pigman!");
-		}
-		else if(rightclick instanceof Enderman && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.enderman", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)58));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a enderman!");
-		}
-		else if(rightclick instanceof CaveSpider && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.cavespider", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)59));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a cave spider!");
-		}
-		else if(rightclick instanceof Blaze && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.blaze", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)61));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a blaze!");
-		}
-		else if(rightclick instanceof Bat && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.bat", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)65));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a bat!");
-		}
-		else if(rightclick instanceof Pig && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.pig", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)90));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a pig!");
-		}
-		else if(rightclick instanceof Sheep && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.sheep", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)91));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a sheep!");
-		}
-		else if(event.getRightClicked().getType() == EntityType.COW && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.cow", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)92));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a cow!");
-		}
-		else if(rightclick instanceof Chicken && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.chicken", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)93));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a chicken!");
-		}
-		else if(rightclick instanceof Squid && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.squid", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)94));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a squid!");
-		}
-		else if(rightclick instanceof Wolf && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.wolf", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)95));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a wolf!");
-		}
-		else if(event.getRightClicked().getType() == EntityType.MUSHROOM_COW && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.mooshroom", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)96));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a cow with the mad cow desease!");
-		}
-		else if(rightclick instanceof Ocelot && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.ocelot", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)98));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a ocelot!");
-		}
-		else if(rightclick instanceof Villager && p.getItemInHand().getType() == Material.STICK && hasPerms("geta.villager", p) && event.getPlayer().isOp()){
-			rightclick.remove();
-			inventory.addItem(new ItemStack(Material.MONSTER_EGG, 1, (short)120));
-			loc.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
-			p.updateInventory();
-			p.sendMessage(ChatColor.GREEN+"You caught a villager!");
-		}
-	}
-	private boolean hasPerms(String perm, Player p){
-		return p.hasPermission(perm);
 	}
 }
